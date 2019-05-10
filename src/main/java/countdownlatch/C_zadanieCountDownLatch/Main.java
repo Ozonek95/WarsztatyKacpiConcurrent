58437e603e221a47f643d8668cdf48cdd5596bc6
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-05-10 08:51:12 +0200

    Implement task C solution

--- src/main/java/countdownlatch/C_zadanieCountDownLatch/Main.java
+++ src/main/java/countdownlatch/C_zadanieCountDownLatch/Main.java
@@ -1,5 +1,13 @@
 package countdownlatch.C_zadanieCountDownLatch;
 
+import fabryczkapomocnicza.MyThreadFactory;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * <p>Czas na pierwsze większe zadanko dla Ciebie :) Będzie ono bardzo podobne do zadania z przykładu,
  * jednak dla własnego dobra staraj się tam nie zaglądać. Użyj CountDownLatch, poczytaj dokumentację jeżeli
@@ -13,9 +21,82 @@ public class Main {
     //TODO:5 kierowców otrzymało smsa z miejscem rozpoczęcia, czym prędzej jadą na miejsce! Jednak każdy jest w innym
     //TODO:miejscu miasta, więc czas dotarcia będzie różny. Dopiero kiedy wszyscy pojawią się na miejscu - wyścig ruszy!
     //TODO:Skorzystaj śmiało z fabryki wątków, którą umieściłem w projekcie.
-    //TODO:Kiedy skończysz, spróbuj napisać program tak, że po jakimś określonym czasie wyścig rozpocznie się niezależnie od tego, czy wszyscy zdążyli się stawić na miejscu :)
+
+    static final int LICZBA_UCZESTNIKÓW = 5;
 
     public static void main(String[] args) {
+        CountDownLatch latch = new CountDownLatch(LICZBA_UCZESTNIKÓW);
+        Race race = new Race(latch);
+        new Thread(race, "Wyścig").start();
 
+        ThreadFactory threadFactory = new MyThreadFactory("Kierowca");
+        ExecutorService executorService = Executors.newFixedThreadPool(5, threadFactory);
+        for(int i = 0; i < LICZBA_UCZESTNIKÓW; i++) {
+            executorService.submit(new Driver(latch, race));
+        }
+        executorService.shutdown();
     }
+}
+
+class Race implements Runnable {
+    private final CountDownLatch latch;
+    private final CopyOnWriteArrayList<Driver> driversReadyToStart = new CopyOnWriteArrayList<>();
+
+    public Race(CountDownLatch countDownLatch) {
+        this.latch = countDownLatch;
+    }
+
+    @Override
+    public void run() {
+        organiseRace();
+    }
+
+    void organiseRace() {
+        System.out.println("Wysyłam info o wyścigu. Czekamy na uczestników");
+        try {
+            latch.await();
+            System.out.println("Wszyscy są, ruszamy");
+        } catch (InterruptedException e) {
+            System.err.println("Wpadła policja koniec imprezy");
+        }
+    }
+
+    void addDriver(Driver driver) {
+        driversReadyToStart.add(driver);
+    }
+}
+
+class Driver implements Runnable {
+    private final CountDownLatch latch;
+    private final Race race;
+
+    Driver(CountDownLatch latch, Race race) {
+        this.latch = latch;
+        this.race = race;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Dostałem sms o wyścigu. Wyruszam na start "
+            + Thread.currentThread().getName());
+        driveToStartLine();
+        comeToStartLine();
+    }
+
+    void driveToStartLine() {
+        System.out.println("Jadę na start " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(ThreadLocalRandom.current().nextInt(1000));
+        } catch (InterruptedException e) {
+            System.err.println("Zepsuło mi się auto " + Thread.currentThread().getName());
+        }
+    }
+
+    void comeToStartLine() {
+        System.out.println("Dojechałem na start i jestem gotowy "
+            + Thread.currentThread().getName());
+        race.addDriver(this);
+        latch.countDown();
+    }
+
 }
