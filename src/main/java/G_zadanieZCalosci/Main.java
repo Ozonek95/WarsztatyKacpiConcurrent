25baf8ee10aff095789f9b824387f7a9ccc3a372
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-05-13 09:49:29 +0200

    Add task G solution explanation

--- src/main/java/G_zadanieZCalosci/Main.java
+++ src/main/java/G_zadanieZCalosci/Main.java
@@ -3,9 +3,18 @@ package G_zadanieZCalosci;
 import java.util.List;
 
 /**
+ * Zadanie wykorzystujące barierę i countDownLatch.
+ * Firma tworzy wyścigi multiplayer. 10 graczy bierze udział w teście na temat
+ * poprzedniej wersji gry. 5 pierwszych osób, które go rozwiążą, będą mogli
+ * rozegrać 3 rudny we wstępną wersję nowej części!
+ *
  * @author Kacper Staszek
  */
 class Main {
+  
+  //TODO: 10 graczy jednocześnie rozpoczyna test. 5 pierwszych może rozegrać 3 wyścigi przeciwko sobie
+  //TODO: Pozostałych 5 graczy może skończyć rozwiązywać test.
+  //TODO: program kończy się, po rozegraniu 3 wyścigów dema. Wyścig kończy się, kiedy wszyscy gracze dojadą na metę.
 
   public static void main(String[] args) {
 
