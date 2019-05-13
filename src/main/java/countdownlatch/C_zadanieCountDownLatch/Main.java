af557e420797fba6e577daa84720e1f61e2be384
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-05-13 10:31:19 +0200

    Create skeleton with tests to task C

--- src/main/java/countdownlatch/C_zadanieCountDownLatch/Main.java
+++ src/main/java/countdownlatch/C_zadanieCountDownLatch/Main.java
@@ -1,112 +1,78 @@
 package countdownlatch.C_zadanieCountDownLatch;
 
-import fabryczkapomocnicza.MyThreadFactory;
-
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * <p>Czas na pierwsze większe zadanko dla Ciebie :) Będzie ono bardzo podobne do zadania z przykładu,
- * jednak dla własnego dobra staraj się tam nie zaglądać. Użyj CountDownLatch, poczytaj dokumentację jeżeli
- * nie jesteś czegoś pewien. Jeżeli zrozumiałeś koncept, z zadaniem powinieneś sobie poradzić sam :)</p>
+ * <p>Czas na pierwsze większe zadanko dla Ciebie :) Będzie ono bardzo podobne do zadania z
+ * przykładu, jednak dla własnego dobra staraj się tam nie zaglądać. Użyj CountDownLatch, poczytaj
+ * dokumentację jeżeli nie jesteś czegoś pewien. Jeżeli zrozumiałeś koncept, z zadaniem powinieneś
+ * sobie poradzić sam :)</p>
  *
  * @author Kacper Staszek
  */
 public class Main {
 
-    //TODO: Napisz program, który symuluje NIELEGALNE WYSCIGI W LA! Coś jak Need For Speed ;D
-    //TODO:5 kierowców otrzymało smsa z miejscem rozpoczęcia, czym prędzej jadą na miejsce! Jednak każdy jest w innym
-    //TODO:miejscu miasta, więc czas dotarcia będzie różny. Dopiero kiedy wszyscy pojawią się na miejscu - wyścig ruszy!
-    //TODO:Skorzystaj śmiało z fabryki wątków, którą umieściłem w projekcie.
+  //TODO: Napisz program, który symuluje NIELEGALNE WYSCIGI W LA! Coś jak Need For Speed ;D
+  //TODO:5 kierowców otrzymało smsa z miejscem rozpoczęcia, czym prędzej jadą na miejsce! Jednak każdy jest w innym
+  //TODO:miejscu miasta, więc czas dotarcia będzie różny. Dopiero kiedy wszyscy pojawią się na miejscu - wyścig ruszy!
+  //TODO:Skorzystaj śmiało z fabryki wątków, którą umieściłem w projekcie.
 
-    static final int LICZBA_UCZESTNIKÓW = 5;
+  static final int LICZBA_UCZESTNIKÓW = 5;
 
-    public static void main(String[] args) {
-        CountDownLatch latch = new CountDownLatch(LICZBA_UCZESTNIKÓW);
-        Race race = new Race(latch);
-        new Thread(race, "Wyścig").start();
+  public static void main(String[] args) {
 
-        ThreadFactory threadFactory = new MyThreadFactory("Kierowca");
-        ExecutorService executorService = Executors.newFixedThreadPool(5, threadFactory);
-        for (int i = 0; i < LICZBA_UCZESTNIKÓW; i++) {
-            executorService.submit(new Driver(latch, race));
-        }
-        executorService.shutdown();
-    }
+  }
 }
 
 class Race implements Runnable {
-    private final CountDownLatch latch;
-    private final CopyOnWriteArrayList<Driver> driversReadyToStart = new CopyOnWriteArrayList<>();
 
-    Race(CountDownLatch countDownLatch) {
-        this.latch = countDownLatch;
-    }
+  private final CopyOnWriteArrayList<Driver> driversReadyToStart = new CopyOnWriteArrayList<>();
 
-    @Override
-    public void run() {
-        organiseRace();
-    }
+  Race(CountDownLatch countDownLatch) {
 
-    void organiseRace() {
-        try {
-            latch.await();
-            startRace();
-        } catch (InterruptedException e) {
-            System.err.println(e.getMessage());
-        }
-    }
+  }
 
-    void startRace() {
-        System.out.println("Wszyscy są, ruszamy");
-    }
+  @Override
+  public void run() {
+    organiseRace();
+  }
 
-    void addDriver(Driver driver) {
-        driversReadyToStart.add(driver);
-    }
+  void organiseRace() {
+    System.out.println("Wysyłam info o wyścigu. Czekamy na uczestników");
+//        Pomyślcie czy czegoś tu nie brakuje
+    System.out.println("Wszyscy są, ruszamy");
+  }
 
-    int getDriversReadyToStart() {
-        return driversReadyToStart.size();
-    }
+  void addDriver(Driver driver) {
+
+  }
+
+  int getDriversReadyToStart() {
+    return driversReadyToStart.size();
+  }
 
 }
 
 class Driver implements Runnable {
-    private final CountDownLatch latch;
-    private final Race race;
 
-    Driver(CountDownLatch latch, Race race) {
-        this.latch = latch;
-        this.race = race;
-    }
+  Driver(CountDownLatch latch, Race race) {
 
-    @Override
-    public void run() {
-        System.out.println("Dostałem sms o wyścigu. Wyruszam na start "
-                + Thread.currentThread().getName());
-        driveToStartLine();
-        comeToStartLine();
-    }
+  }
 
-    void driveToStartLine() {
-        System.out.println("Jadę na start " + Thread.currentThread().getName());
-        try {
-            Thread.sleep(ThreadLocalRandom.current().nextInt(1000,2000));
-        } catch (InterruptedException e) {
-            System.err.println("Zepsuło mi się auto " + Thread.currentThread().getName());
-        }
-    }
+  @Override
+  public void run() {
+    System.out.println("Dostałem sms o wyścigu. Wyruszam na start ");
+    driveToStartLine();
+    comeToStartLine();
+  }
 
-    void comeToStartLine() {
-        System.out.println("Dojechałem na start i jestem gotowy "
-                + Thread.currentThread().getName());
-        latch.countDown();
-        race.addDriver(this);
+  void driveToStartLine() {
+    System.out.println("Jadę na start " + Thread.currentThread().getName());
+  }
 
-    }
-
+  void comeToStartLine() {
+    System.out.println("Dojechałem na start i jestem gotowy ");
+  }
 }
+
--- src/test/java/countdownlatch/C_zadanieCountDownLatch/RaceTest.java
+++ src/test/java/countdownlatch/C_zadanieCountDownLatch/RaceTest.java
@@ -9,7 +9,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,58 +16,58 @@ import org.testng.annotations.Test;
 @Test
 public class RaceTest {
 
-    private Race race;
-    private CountDownLatch latch;
-    private static final int NUMBER_OF_DRIVERS = 5;
+  private Race race;
+  private CountDownLatch latch;
+  private static final int NUMBER_OF_DRIVERS = 5;
 
-    @BeforeMethod
-    public synchronized void init(){
-        latch = new CountDownLatch(NUMBER_OF_DRIVERS);
-        race = new Race(latch);
-        new Thread(race,"Wyścig").start();
-        ThreadFactory threadFactory = new MyThreadFactory("Kierowca");
-        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_DRIVERS, threadFactory);
-        for(int i = 0; i < NUMBER_OF_DRIVERS; i++) {
-            executorService.submit(new Driver(latch, race));
-        }
-        executorService.shutdown();
+  @BeforeMethod
+  public synchronized void init() {
+    latch = new CountDownLatch(NUMBER_OF_DRIVERS);
+    race = new Race(latch);
+    new Thread(race, "Wyścig").start();
+    ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_DRIVERS,
+        new MyThreadFactory("Kierowca"));
+    for (int i = 0; i < NUMBER_OF_DRIVERS; i++) {
+      executorService.submit(new Driver(latch, race));
     }
+    executorService.shutdown();
+  }
 
-    @Test(invocationCount = 10, threadPoolSize = 10)
-    public void testIfRaceIsEmptyAtBegin() {
-        Assert.assertEquals(0, race.getDriversReadyToStart());
-        await().until(race::getDriversReadyToStart,equalTo(NUMBER_OF_DRIVERS));
-        await().until(latch::getCount,equalTo(0L));
-        Assert.assertEquals(latch.getCount(), 0L);
-    }
+  @Test(invocationCount = 10, threadPoolSize = 10)
+  public void testIfRaceIsEmptyAtBegin() {
+    Assert.assertEquals(0, race.getDriversReadyToStart());
+    await().until(race::getDriversReadyToStart, equalTo(NUMBER_OF_DRIVERS));
+    await().until(latch::getCount, equalTo(0L));
+    Assert.assertEquals(latch.getCount(), 0L);
+  }
 
-    @Test(invocationCount = 10, threadPoolSize = 10)
-    public void testIfRaceReachAllStarters() throws Exception {
-        await().until(fieldIn(race).ofType(CopyOnWriteArrayList.class)
-            .andWithName("driversReadyToStart")
-            .call()::size, equalTo(NUMBER_OF_DRIVERS));
-    }
+  @Test(invocationCount = 10, threadPoolSize = 10)
+  public void testIfRaceReachAllStarters() throws Exception {
+    await().until(fieldIn(race).ofType(CopyOnWriteArrayList.class)
+        .andWithName("driversReadyToStart")
+        .call()::size, equalTo(NUMBER_OF_DRIVERS));
+  }
 
-    @Test(invocationCount = 10, threadPoolSize = 10)
-    public void testIfRaceLatchIsFiveAtBeginning(){
-        Assert.assertEquals(NUMBER_OF_DRIVERS, latch.getCount());
-    }
+  @Test(invocationCount = 10, threadPoolSize = 10)
+  public void testIfRaceLatchIsFiveAtBeginning() {
+    Assert.assertEquals(NUMBER_OF_DRIVERS, latch.getCount());
+  }
 
-    @Test(invocationCount = 10, threadPoolSize = 10)
-    public void testIfRaceLatchIsZeroAfterFinish(){
-        await().until(latch::getCount,equalTo((long) NUMBER_OF_DRIVERS));
-    }
+  @Test(invocationCount = 10, threadPoolSize = 10)
+  public void testIfRaceLatchIsZeroAfterFinish() {
+    await().until(latch::getCount, equalTo((long) NUMBER_OF_DRIVERS));
+  }
 
-    @Test(invocationCount = 10, threadPoolSize = 10)
-    public void testIfRaceHaveFiveDriversAfterLatchIsZero(){
-        await().until(latch::getCount,equalTo(0L));
-        await().until(race::getDriversReadyToStart,equalTo(NUMBER_OF_DRIVERS));
-        Assert.assertEquals(race.getDriversReadyToStart(), NUMBER_OF_DRIVERS);
-    }
+  @Test(invocationCount = 10, threadPoolSize = 10)
+  public void testIfRaceHaveFiveDriversAfterLatchIsZero() {
+    await().until(latch::getCount, equalTo(0L));
+    await().until(race::getDriversReadyToStart, equalTo(NUMBER_OF_DRIVERS));
+    Assert.assertEquals(race.getDriversReadyToStart(), NUMBER_OF_DRIVERS);
+  }
 
-    @Test(invocationCount = 10, threadPoolSize = 10)
-    public void testIfBeforeLatchIsZeroDriversIsNotFive(){
-        await().until(() -> latch.getCount() > 0, equalTo(true));
-        Assert.assertTrue(race.getDriversReadyToStart()<NUMBER_OF_DRIVERS);
-    }
+  @Test(invocationCount = 10, threadPoolSize = 10)
+  public void testIfBeforeLatchIsZeroDriversIsNotFive() {
+    await().until(() -> latch.getCount() > 0, equalTo(true));
+    Assert.assertTrue(race.getDriversReadyToStart() < NUMBER_OF_DRIVERS);
+  }
 }
