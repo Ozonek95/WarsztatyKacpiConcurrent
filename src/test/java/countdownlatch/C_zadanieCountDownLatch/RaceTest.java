bf1788e6c636646ad4fa7c091e8a4c3ecddde63a
Ozonek95 <kacperstaszek1995@gmail.com>
2019-05-13 09:20:18 +0200

    Changes in test
    Remove magic number and create static variable NUMBER_OF_DRIVERS.

--- src/test/java/countdownlatch/C_zadanieCountDownLatch/RaceTest.java
+++ src/test/java/countdownlatch/C_zadanieCountDownLatch/RaceTest.java
@@ -1,70 +1,74 @@
 package countdownlatch.C_zadanieCountDownLatch;
 
-import fabryczkapomocnicza.MyThreadFactory;
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import java.util.concurrent.*;
-
 import static org.awaitility.Awaitility.await;
 import static org.awaitility.Awaitility.fieldIn;
 import static org.hamcrest.CoreMatchers.equalTo;
+
+import fabryczkapomocnicza.MyThreadFactory;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 @Test
 public class RaceTest {
 
     private Race race;
     private CountDownLatch latch;
+    private static final int NUMBER_OF_DRIVERS = 5;
 
     @BeforeMethod
     public synchronized void init(){
-        latch = new CountDownLatch(5);
+        latch = new CountDownLatch(NUMBER_OF_DRIVERS);
         race = new Race(latch);
         new Thread(race,"Wyścig").start();
         ThreadFactory threadFactory = new MyThreadFactory("Kierowca");
-        ExecutorService executorService = Executors.newFixedThreadPool(5, threadFactory);
-        for(int i = 0; i < 5; i++) {
+        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_DRIVERS, threadFactory);
+        for(int i = 0; i < NUMBER_OF_DRIVERS; i++) {
             executorService.submit(new Driver(latch, race));
         }
         executorService.shutdown();
     }
 
-    @Test(invocationCount = 5, threadPoolSize = 5)
+    @Test(invocationCount = 10, threadPoolSize = 10)
     public void testIfRaceIsEmptyAtBegin() {
         Assert.assertEquals(0, race.getDriversReadyToStart());
-        await().until(race::getDriversReadyToStart,equalTo(5));
+        await().until(race::getDriversReadyToStart,equalTo(NUMBER_OF_DRIVERS));
         await().until(latch::getCount,equalTo(0L));
         Assert.assertEquals(latch.getCount(), 0L);
     }
 
-    @Test(invocationCount = 5, threadPoolSize = 5)
+    @Test(invocationCount = 10, threadPoolSize = 10)
     public void testIfRaceReachAllStarters() throws Exception {
         await().until(fieldIn(race).ofType(CopyOnWriteArrayList.class)
-                .andWithName("driversReadyToStart").call()::size,equalTo(5));
+            .andWithName("driversReadyToStart")
+            .call()::size, equalTo(NUMBER_OF_DRIVERS));
     }
 
-    @Test(invocationCount = 5, threadPoolSize = 5)
+    @Test(invocationCount = 10, threadPoolSize = 10)
     public void testIfRaceLatchIsFiveAtBeginning(){
-        Assert.assertEquals(5, latch.getCount());
+        Assert.assertEquals(NUMBER_OF_DRIVERS, latch.getCount());
     }
 
-    @Test(invocationCount = 5, threadPoolSize = 5)
+    @Test(invocationCount = 10, threadPoolSize = 10)
     public void testIfRaceLatchIsZeroAfterFinish(){
-        await().until(latch::getCount,equalTo(0L));
+        await().until(latch::getCount,equalTo((long) NUMBER_OF_DRIVERS));
     }
 
-    @Test(invocationCount = 5, threadPoolSize = 5)
+    @Test(invocationCount = 10, threadPoolSize = 10)
     public void testIfRaceHaveFiveDriversAfterLatchIsZero(){
         await().until(latch::getCount,equalTo(0L));
-        await().until(race::getDriversReadyToStart,equalTo(5));
-        Assert.assertEquals(5, race.getDriversReadyToStart());
+        await().until(race::getDriversReadyToStart,equalTo(NUMBER_OF_DRIVERS));
+        Assert.assertEquals(race.getDriversReadyToStart(), NUMBER_OF_DRIVERS);
     }
 
-    @Test(invocationCount = 5, threadPoolSize = 5)
+    @Test(invocationCount = 10, threadPoolSize = 10)
     public void testIfBeforeLatchIsZeroDriversIsNotFive(){
-        await().until(latch::getCount,equalTo(2L));
-        Assert.assertTrue(race.getDriversReadyToStart()<5);
-        System.out.println();
+        await().until(() -> latch.getCount() > 0, equalTo(true));
+        Assert.assertTrue(race.getDriversReadyToStart()<NUMBER_OF_DRIVERS);
     }
 }
