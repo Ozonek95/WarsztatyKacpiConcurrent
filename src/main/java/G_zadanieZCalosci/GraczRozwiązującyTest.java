254d76718114b0196183cd1777238070cb55fb62
Ozonek95 <kacperstaszek1995@gmail.com>
2019-05-14 15:14:28 +0200

    Fix task G solution

--- src/main/java/G_zadanieZCalosci/GraczRozwiązującyTest.java
+++ src/main/java/G_zadanieZCalosci/GraczRozwiązującyTest.java
@@ -8,9 +8,12 @@ import java.util.concurrent.ThreadLocalRandom;
  * @author Kacper Staszek
  */
 class GraczRozwiązującyTest implements Runnable {
+  private final CountDownLatch latch;
+  private final TestZGry testZGry;
 
   GraczRozwiązującyTest(CountDownLatch latch, TestZGry testZGry) {
-
+    this.latch = latch;
+    this.testZGry = testZGry;
   }
 
   GraczGrającyWGrę przeobraź(){
@@ -19,10 +22,14 @@ class GraczRozwiązującyTest implements Runnable {
   @Override
   public void run() {
     System.out.println("Zaczynam rozwiązywać test "+this);
-
-    System.out.println("ROZWIĄZAŁEM! "+this);
-
-    //Co jeżeli 5 graczy zdąży już rozwiązać test?
+    try {
+      Thread.sleep(ThreadLocalRandom.current().nextInt(5000));
+      System.out.println("ROZWIĄZAŁEM! "+this);
+      latch.countDown();
+      testZGry.getListaGraczyKtórzyPrzeszli().add(this.przeobraź());
+    } catch (InterruptedException e) {
+      return;
+    }
   }
 
   @Override
