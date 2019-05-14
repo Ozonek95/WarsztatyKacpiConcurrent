13a705533522b4187fee7bca0bf9dd49f239ed53
Ozonek95 <kacperstaszek1995@gmail.com>
2019-05-14 12:42:06 +0200

    Create phaseBarrier package, witch shows simple Phaser usage.

---
+++ src/main/java/phaser/H_wprowadzeniePhaser/phaserBarrier/Zadanie.java
@@ -0,0 +1,38 @@
+package phaser.H_wprowadzeniePhaser.phaserBarrier;
+
+import java.util.concurrent.Phaser;
+
+/**
+ * Abstrakcja do tworzenia konkretnych typów zadań.
+ *
+ * @author Kacper Staszek
+ */
+abstract class Zadanie implements  Runnable {
+  final Phaser phaser;
+  private final int ilośćCykliDoWykonania;
+  boolean jobDone = false;
+
+  /**
+   * @param phaser do niego odnosiła się będzie logika cyklicznego wykonania zadania.
+   * @param ilośćCykliDoWykonania po określonej fazie, zadanie nie będzie dłużej wykonywane.
+   */
+  Zadanie(Phaser phaser, int ilośćCykliDoWykonania) {
+    this.phaser = phaser;
+    this.ilośćCykliDoWykonania = ilośćCykliDoWykonania;
+  }
+
+  /**
+   * Jeżeli dana faza Phasera jest równa ilości cyklów do wykonania, zadanie skończy się wykonywać i wyrejestruje się z Phasera.
+   * inaczej będzie kontynuowane.
+   *
+   */
+  void sprawdźCzyKontynuować() {
+    if (phaser.getPhase() == ilośćCykliDoWykonania) {
+      System.out.println("Wyrejestrowuje się");
+      phaser.arriveAndDeregister();
+      jobDone=true;
+    } else {
+      phaser.arriveAndAwaitAdvance();
+    }
+  }
+}
