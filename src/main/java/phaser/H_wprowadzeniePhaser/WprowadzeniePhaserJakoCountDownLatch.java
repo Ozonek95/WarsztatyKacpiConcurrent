dad341e2ef56f8a42ed45fc75b5b9e64de3f1596
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-05-14 10:27:11 +0200

    Create package to Phaser class workshop

---
+++ src/main/java/phaser/H_wprowadzeniePhaser/WprowadzeniePhaserJakoCountDownLatch.java
@@ -0,0 +1,11 @@
+package phaser.H_wprowadzeniePhaser;
+
+/**
+ * @author Marcin Ogorzalek
+ */
+public class WprowadzeniePhaserJakoCountDownLatch {
+
+  public static void main(String[] args) {
+
+  }
+}
